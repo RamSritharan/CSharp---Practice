e77b2830bff817d44b40146ee62e7a1a1977bbbf
RamSritharan <ramkishan1795@gmail.com>
2023-05-10 02:06:32 -0400

    First commit

--- Practice C Sharp/Program.cs
+++ Practice C Sharp/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    { static void Main()
+        {
+            Console.WriteLine("Hello Worldfcc!");
+        }
+    }
+}
+
 
